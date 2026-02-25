8be091b2b5c1c8ef01c668c1b45189a6204545b7
Hriday Devkar <hridaydevkar@gmail.com>
2026-02-25 21:53:42 +0530

    remove github actions, revert config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: "export",
-  basePath: "/habit-tracker",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
